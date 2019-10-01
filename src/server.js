5872e545816d5a9a01cec3c6915d0f7f5dbdb421
Matheus Mattana <matheusmattana@hotmail.com>
2019-10-01 11:57:26 -0300

    First Commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição )
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
